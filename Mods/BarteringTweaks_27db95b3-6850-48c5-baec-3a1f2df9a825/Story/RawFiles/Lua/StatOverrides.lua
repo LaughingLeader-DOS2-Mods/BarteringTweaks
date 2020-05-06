1e7baecb8eb6eba569a190275631a19358c5173e
LaughingLeader <laughingleader@gmail.com>
2020-05-05 21:30:00 -0500

    Updated extender support, added missing dialog handles

---
+++ Mods/BarteringTweaks_27db95b3-6850-48c5-baec-3a1f2df9a825/Story/RawFiles/Lua/StatOverrides.lua
@@ -0,0 +1,11 @@
+local function ModuleLoading()
+    --Kalavinkas_Combat_Enhanced_e844229e-b744-4294-9102-a7362a926f71
+    if Ext.IsModLoaded("e844229e-b744-4294-9102-a7362a926f71") then
+		Ext.Print("[BarteringTweaks] Divinity Unleashed detected. Changing the Pet Pal book's description.")
+		--Original:
+		--6d71b1e0-4150-47fe-bc77-83d9b3efcb77
+		Ext.StatSetAttribute("BOOK_LLBARTER_PetPalTalentBook", "RootTemplate", "f31d4498-59a5-4f04-ba96-89a750111ed7")
+	end
+end
+
+Ext.RegisterListener("ModuleLoading", ModuleLoading)
