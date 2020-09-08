ffabd19293c14fc94d6f318cb2c7e28edcb47d60
LaughingLeader <laughingleader@gmail.com>
2020-09-08 03:57:47 -0500

    Reworking Lua scripts, disabling sneak tweaks to off by default

--- Mods/BarteringTweaks_27db95b3-6850-48c5-baec-3a1f2df9a825/Story/RawFiles/Lua/BootstrapClient.lua
+++ Mods/BarteringTweaks_27db95b3-6850-48c5-baec-3a1f2df9a825/Story/RawFiles/Lua/BootstrapClient.lua
@@ -1,1 +1,28 @@
 Ext.Require("StatOverrides.lua")
+
+local function FixDiscountText(amount)
+	local ui = Ext.GetBuiltinUI("Public/Game/GUI/trade.swf")
+	if ui ~= nil then
+		--public function setText(param1:Number, param2:String) : *
+		--this.trade_mc.mcRepContainer.attitude_txt -- is index 2
+		if type(amount) == "number" then
+			ui:Invoke("setText", 2, tostring(math.ceil(amount)))
+		else
+			ui:Invoke("setText", 2, amount)
+		end
+	end
+end
+
+Ext.RegisterNetListener("LLBARTER_FixDiscountText", function(cmd, amountstr)
+	FixDiscountText(amountstr)
+end)
+
+Ext.RegisterListener("SessionLoaded", function()
+	---@param ui UIObject
+	Ext.RegisterUINameCall("overItem", function(ui, ...)
+		print("Trade window type:", ui:GetTypeId(), Ext.JsonStringify({...}))
+	end)
+	Ext.RegisterUINameInvokeListener("setText", function(ui, ...)
+		print("Trade window type:", ui:GetTypeId(), Ext.JsonStringify({...}))
+	end)
+end)
--- Mods/BarteringTweaks_27db95b3-6850-48c5-baec-3a1f2df9a825/Story/RawFiles/Lua/BootstrapServer.lua
+++ Mods/BarteringTweaks_27db95b3-6850-48c5-baec-3a1f2df9a825/Story/RawFiles/Lua/BootstrapServer.lua
@@ -1,1 +1,90 @@
 Ext.Require("StatOverrides.lua")
+
+---@type ModSettings
+local settings = nil
+-- LeaderLib
+if Ext.IsModLoaded("7e737d2f-31d2-4751-963f-be6ccc59cd0c") then
+	settings = Ext.Require("LeaderLibSettings.lua")
+	Ext.RegisterListener("SessionLoaded", function()
+		Mods.LeaderLib.SettingsManager.AddSettings(settings)
+	end)
+
+end
+
+function SetSneakingTweaksDisabled(username)
+	if settings ~= nil then
+		settings.Global.Flags.LLBARTER_SneakingTweaksDisabled.Targets[username] = true
+	end
+end
+
+function SetSneakingTweaksEnabled(username)
+	if settings ~= nil then
+		settings.Global.Flags.LLBARTER_SneakingTweaksDisabled.Targets[username] = nil
+	end
+end
+
+
+function FixDiscountText(uuid, amount)
+	Ext.PostMessageToClient(uuid, "LLBARTER_FixDiscountText", amount)
+end
+
+function ClearOldGlobalSettings()
+	Osi.DB_LeaderLib_ModApi_GlobalSettings_Register_GlobalFlag:Delete("27db95b3-6850-48c5-baec-3a1f2df9a825", nil)
+	Osi.DB_LeaderLib_ModApi_GlobalSettings_Register_GlobalFlag:Delete("27db95b3-6850-48c5-baec-3a1f2df9a825", nil, nil)
+	Osi.DB_LeaderLib_GlobalSettings_GlobalFlags:Delete("27db95b3-6850-48c5-baec-3a1f2df9a825", nil)
+	Osi.DB_LeaderLib_GlobalSettings_GlobalFlags:Delete("27db95b3-6850-48c5-baec-3a1f2df9a825", nil, nil)
+end
+
+---@type StatTreasureSubTable
+local EnabledBooksSubtables = {
+	{
+		Amounts = {1},
+		DropCounts = {1},
+		Categories = {
+			{
+				Frequency = 1,
+				TreasureCategory = "I_BOOK_LLBARTER_PetPalTalentBook",
+				Unique = 1
+			},
+		},
+		TotalCount = 1,
+		TotalFrequency = 1
+	},
+	{
+		Amounts = {1},
+		DropCounts = {1},
+		Categories = {
+			{
+				Frequency = 1,
+				TreasureCategory = "I_BOOK_LLBARTER_SettingsBook",
+				Unique = 1
+			}
+		},
+		TotalCount = 1,
+		TotalFrequency = 1
+	}
+}
+
+function DisableBookTreasure()
+	local stat = Ext.GetTreasureTable("ST_BarteringTweaks_Books")
+	if stat ~= nil then
+		stat.SubTables = nil
+		Ext.UpdateTreasureTable(stat)
+	end
+end
+
+function EnableBookTreasure()
+	local stat = Ext.GetTreasureTable("ST_BarteringTweaks_Books")
+	if stat ~= nil then
+		stat.SubTables = EnabledBooksSubtables
+		Ext.UpdateTreasureTable(stat)
+	end
+end
+
+Ext.RegisterOsirisListener("GameStarted", 2, "after", function(region, editorMode)
+	if GlobalGetFlag("LLBARTER_PreventTraderBooksEnabled") == 1 then
+		DisableBookTreasure()
+	else
+		--EnableBookTreasure()
+	end
+end)
